2038182646d574df5fecb6e7607bb4ef524fe0d2
grantkitt <38022110+grantkitt@users.noreply.github.com>
2022-11-30 13:36:27 -0600

    bruh

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import CollectionsHeader from '../components/CollectionsHeader';
 import EmailSection from '../components/emailSection';
 import FancyBrands from '../components/FancyBrands';
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
